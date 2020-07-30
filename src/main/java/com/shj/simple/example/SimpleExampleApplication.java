10952cf5b9a53a98298d1403331e56098a495eed
石豪杰 <shihaojie@syhealth.com>
2020-07-30 14:14:34 +0800

    修改数据源->> mysql

--- src/main/java/com/shj/simple/example/SimpleExampleApplication.java
+++ src/main/java/com/shj/simple/example/SimpleExampleApplication.java
@@ -2,9 +2,6 @@ package com.shj.simple.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class SimpleExampleApplication {
